9a5c7841f58096465337020ce7a9ae62f43c9ffd
ganeshlonare <ganeshlonare311@gmail.com>
2024-01-03 20:51:44 +0530

    Created Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes , Route } from "react-router-dom";
 import Navbar from "./components/Navbar.component";
+import UserAuthForm from "./pages/UserAuthForm.page";
 
 export default function App() {
   return (
-    <Navbar />
+    <Routes>
+      <Route path="/" element={<Navbar />} >
+      <Route path="sign-in" element={<UserAuthForm type={"sign-in"}/>} />
+      <Route path="sign-up" element={<UserAuthForm type={"sign-up"}/>} />
+      </Route>
+    </Routes>
   )
 }
