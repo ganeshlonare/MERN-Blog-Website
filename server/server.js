8ad235dcb6b070fa34890c486146466513f24da5
ganeshlonare <ganeshlonare311@gmail.com>
2024-01-18 12:51:27 +0530

    Created the server and connected to the backend

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config';
+// import authRouter from './routes/auth.route.js'
+
+const app=express();
+let PORT=process.env.PORT;
+
+async function main(){
+   await mongoose.connect(process.env.MONGO)
+}
+
+main()
+.then((res)=>{
+    console.log(`connected to db`)
+})
+.catch((err)=>console.log(err))
+
+app.listen(PORT,()=>{
+    console.log(`Server is working on port ${PORT}`);
+})
+
+app.use(express.json());
+
+
+
+// app.use("/api/auth" , authRouter)
+
