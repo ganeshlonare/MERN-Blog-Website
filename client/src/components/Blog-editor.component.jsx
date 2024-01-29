40fd68d9c7ba1ae085eb4418df2bb4a86b335c84
ganeshlonare <ganeshlonare311@gmail.com>
2024-01-29 23:23:50 +0530

    completed Blog banner image upload functionality

--- client/src/components/Blog-editor.component.jsx
+++ client/src/components/Blog-editor.component.jsx
@@ -2,12 +2,52 @@ import { Link } from 'react-router-dom'
 import logo from '../imgs/logo.png'
 import AnimationWrapper from '../common/Page-animation'
 import defaultBanner from '../imgs/blog banner.png'
+import { useEffect, useState } from 'react'
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from '../common/Firebase'
+import {Toaster , toast } from 'react-hot-toast'
 
 export default function BlogEditor() {
-    const handleBannerUpload=(e)=>{
-        console.log(e.target.files[0]);
-        const img=e.target.files[0];
+
+    const [file , setfile]=useState(undefined);
+    const [fileper,setFilePerc]=useState(0);
+    const [fileUploadError , setFileUploadError]=useState(false);
+    const [formData , setFormData]=useState({});
+
+    let loadingToast=0;
+
+    useEffect(()=>{
+        if(file){
+            handleBannerUpload(file);
+            loadingToast=toast.loading("Uploading...");
+        }
+    },[file]);
+
+    const handleBannerUpload=(file)=>{
+        const storage=getStorage(app);
+        const filename=new Date().getTime()+file.name;
+        const storageRef=ref(storage,filename);
+        const uploadTask=uploadBytesResumable(storageRef,file);
+
+        uploadTask.on('state_changed',
+    (snapshot)=>{
+      const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+      setFilePerc(Math.round(progress));
+    },
+    (error)=>{
+      setFileUploadError(true);
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref)
+      .then((downloadURL)=>{
+        setFormData({...formData,profile_img:downloadURL});
+            toast.success("Image uploaded successfully 👍");
+            toast.dismiss(loadingToast);
+      })  
+    })
+
     }
+    
   return (
     <>
     <nav className='navbar'>
@@ -27,21 +67,21 @@ export default function BlogEditor() {
             </button>
         </div>
     </nav>
-
+    <Toaster />
     <AnimationWrapper>
         <section>
             <div className="mx-auto max-w-[900px] w-full">
 
                 <div className="relative aspect-video bg-white border-4 border-grey hover:opacity-80">
                     <label htmlFor='uploadBanner'>
-                        <img src={defaultBanner} 
+                        <img src={formData.profile_img || defaultBanner} 
                         className='z-20'
                         alt="defaultBanner" />
                         <input
                          type="file" id="uploadBanner" 
                          accept='.pnj , .jpg , .jpeg'
                          hidden 
-                         onChange={handleBannerUpload}
+                         onChange={(e)=>setfile(e.target.files[0])}
                          />
                     </label>
                 </div>
