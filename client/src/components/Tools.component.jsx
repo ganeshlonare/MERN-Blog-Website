0666e351376ff50fef9c9e79b279ea71506c724b
ganeshlonare <ganeshlonare311@gmail.com>
2024-02-02 00:37:07 +0530

    updated tools.component file

--- client/src/components/Tools.component.jsx
+++ client/src/components/Tools.component.jsx
@@ -6,12 +6,49 @@ import Quote from '@editorjs/Quote'
 import Marker from '@editorjs/Marker'
 import InlineCode from '@editorjs/inline-code'
 
+const uploadImageByUrl=(e)=>{
+    let link =new Promise((resolve , reject)=>{
+        try {
+            resolve(e)
+        } catch (error) {
+            reject(error)
+        }
+    })
+
+    return link.then(url=>{
+        return {
+            success:1,
+            file:{ url }
+        }
+    })
+}
+
 export const tools={
     embed:Embed,
-    list:List,
-    image:Image,
-    header:Header,
-    quote:Quote,
+    list:{
+        class:List,
+        inlineToolbar:true
+    },
+    image:{
+        class:Image,
+        config:{
+            uploader:{
+                uploadByUrl:uploadImageByUrl
+            }
+        }
+    },
+    header:{
+        class:Header,
+        config:{
+            placeholder:"Type heading...",
+            levels:[2,3],
+            defaultLevel:2
+        }
+    },
+    quote:{
+            class:Quote,
+            inlineToolbar:true
+        },
     marker:Marker,
     inlineCode: InlineCode
 }
