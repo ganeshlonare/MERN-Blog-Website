34b567231067685282b84a2f2efb841f56125920
ganeshlonare <ganeshlonare311@gmail.com>
2024-06-06 23:16:29 +0530

    fetch the latest blog api by axios

--- client/src/pages/Home.page.jsx
+++ client/src/pages/Home.page.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AnimationWrapper from '../common/Page-animation'
 import InPageNavigation from '../components/Inpage-navigation.component'
+import axios from 'axios'
+import Loader from '../components/Loader.component'
+import BlogPostCard from '../components/Blog-post.component'
 
 export default function HomePage() {
+
+  const [blogs , setBlogs]=useState(null)
+
+  const fetchLatestBlog=()=>{
+    axios.get(`/api/blog/latest-blogs`)
+    .then(({data})=>{
+      setBlogs(data.blogs)
+      console.log(data.blogs)
+    })
+  }
+
+  useEffect(()=>{
+    fetchLatestBlog()
+  },[])
+  
+
+
+
   return (
     <AnimationWrapper>
         <section className='h-cover flex justify-center gap-10 '>
@@ -12,7 +33,17 @@ export default function HomePage() {
             <div className="w-full">
 
                 <InPageNavigation routes={["Home" , "Trending Blogs"]} defaultHidden={["Trending Blogs"]}>
-                  <h1>Latest BLogs Here</h1>
+
+                  <>
+                  {
+                    blogs ==null ? <Loader/>: 
+                    blogs.map((blog,i)=>{
+                        return <AnimationWrapper transition={{duration:1 , delay:i*.1}} key={i}><BlogPostCard content={blog} author={blog.author.personal_info}/></AnimationWrapper>
+                    })
+
+                  }
+                  </>
+                  
                   <h1>Trending BLogs Here</h1>
                 </InPageNavigation>
 
